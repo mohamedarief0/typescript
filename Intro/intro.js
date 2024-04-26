6fe286ec05ea99ba7f88e5f9dcd41532e23e4726
Arief <mrarief0110@gmail.com>
2024-04-26 15:45:04 +0530

    Added simple modification for exampling how error works

--- Intro/intro.js
+++ Intro/intro.js
@@ -1,5 +1,7 @@
+// Most of the code inter face are look like an Javascript
+
 var user = { name: "Arief", age: 11 };
 console.log("Arief");
 console.log(user.name);
-//
+// Error assign
 // console.log(user.email);
